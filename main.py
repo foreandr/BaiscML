b6b43e8f8789a2d32acaad77924563477443cdbe
Andre <foreandr@gmail.com>
2021-11-21 17:18:36 -0500

    working to line 21

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ from sklearn import linear_model # SCIKIT LEARN LIBRARY
 
 # LOAD CSV IN PANDAS DATAFRAME
 df = pd.read_csv("testfile.csv")
-print(df)
+#first_column = df.iloc[:, 0] # MAKES NO ESNE?
+second_column = df.iloc[:, 1]
+# Plot scatterpot
+
+
+plt.xlabel("")
+plt.ylabel("")
+plt.scatter(df.area, second_column, color ='red') #
+plt.show()
+
+reg = linear_model.LinearRegression() # object for linear regression
+reg.fit(df[['area']], second_column) # TRAIN linear regression model
