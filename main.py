b24e3314e1a72101f35c072acfd3e6a6320a4ae6
Andre <foreandr@gmail.com>
2021-11-21 19:40:23 -0500

    working gradient descent
    pickle mdel

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model  # SCIKIT LEARN LIBRARY
 import math
 
+df = pd.read_csv("testfile2.csv")
+df.columns = df.columns.str.strip()
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # rRemove NAN column
+# print(df)
 
+# One hot encoding
+dummies = pd.get_dummies((df.town))
+# print(dummies)
 
+# Put dataframes togehter
+merged = pd.concat([df, dummies], axis='columns')
+print(merged)
+
+# Drop original town column
+# DROP 1 DUMMY VARIABLE ALWAYS ??
+final = merged.drop(['town', 'robbinsville'], axis='columns')
+print(final)
