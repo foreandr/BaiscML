afcb5dc21414e0e6284167f2031fc7d92974bda2
Andre <foreandr@gmail.com>
2021-11-21 20:03:24 -0500

    working gradient descent
    pickle model

--- main.py
+++ main.py
@@ -7,33 +7,3 @@ from sklearn import linear_model  # SCIKIT LEARN LIBRARY
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 import math
-
-df = pd.read_csv("testfile2.csv")
-df.columns = df.columns.str.strip()
-df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # Remove NAN column
-# print(df)
-
-# One hot encoding
-dummies = pd.get_dummies((df.town))
-# print(dummies)
-
-# Put dataframes togehter
-merged = pd.concat([df, dummies], axis='columns')
-#print(merged)
-
-# Drop original town column
-# DROP 1 DUMMY VARIABLE ALWAYS ??
-final = merged.drop(['town', '\'west windsor\''], axis='columns') # NOTICE CHANGE IN SECOND TEST
-#print(final)
-
-model = linear_model.LinearRegression()
-x = final.drop('price', axis='columns')
-x = x.loc[:, ~df.columns.str.contains('^Unnamed')]  # Remove NAN column
-#print(x)
-y = final.price
-model.fit(x, y)
-#print(model.predict([[3400, 0, 0]]))
-#print(model.score(x, y)) # ACCURACY %
-
-
-#------------------------------------------------------
