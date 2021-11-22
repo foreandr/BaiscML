51015417770e3bc70acf45b67a295da69e8e49b2
Andre <foreandr@gmail.com>
2021-11-21 22:01:46 -0500

    logistic number guess

--- main.py
+++ main.py
@@ -18,38 +18,6 @@ import math
 # from yahoo_fin.stock_info import get_data
 import yfinance
 
-digits = load_digits()
-
-#print(digits.data[0])
-
-# SHOW 8 BY 8 IMAGE
-#plt.gray()
-#plt.matshow(digits.images[0])
-#plt.show()
-
-
-x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.2)
-model = linear_model.LogisticRegression(max_iter=10000)
-model.fit(x_train, y_train)
-y_predicted = model.predict(x_test)
-
-cm = confusion_matrix(y_test, y_predicted) # WTF IS THIS?
-plt.figure(figsize=(10,7))
-sn.heatmap(cm, annot=True)
-plt.xlabel("prdicted")
-plt.ylabel('Truth')
-plt.show()
-
-print(model.score(x_test,y_test))
-
-#print(digits.data[0:10])
-print(model.predict(digits.data[0:10]))
-
-
-
-
-
-
 
 
 
