c00140eac17b556c1c8141b5d22884fa1e3bc601
Andre <foreandr@gmail.com>
2021-11-22 19:15:38 -0500

    SVM

--- ML9-Random-Forest-Walk.py
+++ ML9-Random-Forest-Walk.py
@@ -1,25 +1,7 @@
-# import tensorflow
-import sklearn.linear_model
-import torch
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn import linear_model  # SCIKIT LEARN LIBRARY
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
-from sklearn.metrics import confusion_matrix
-from sklearn.datasets import load_iris
-from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-
-import seaborn as sn
-import math
-# import yahoo_fin.stock_info as si
-# from yahoo_fin.stock_info import get_data
-import yfinance
 
 
 digits = load_digits()
@@ -31,6 +13,3 @@ x_train, x_test, y_train, y_test = train_test_split(df.drop(['target'],axis='col
 model = RandomForestClassifier(n_estimators=100) # can change to all kinds of things
 model.fit(x_train, y_train)
 print(model.score(x_test, y_test))
-
-
-
