226d69149a6dbb10831304e118646aa0c11fa204
Andre <foreandr@gmail.com>
2021-11-25 12:54:14 -0500

    reddit sent and stock sent

--- PROJECT-2/main.py
+++ PROJECT-2/main.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 import helper
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
@@ -5,7 +7,7 @@ import nltk
 from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
-
+import matplotlib
 
 finviz_URL = 'https://finviz.com/quote.ashx?t='
 tickers=['AMZN', 'MSFT', 'AMD']
@@ -30,10 +32,28 @@ amzn_data = news_tables['AMZN']
 amzn_rows = amzn_data.findAll('tr')# not sure what tr is
 #print(amzn_rows)
 
-
+tempList = [] # it will have to be a list of list of list - TENSOR BABY
+ # next dataframe will also have ticker
 for i in amzn_rows:
     text = (i.a.text) # go through the html tag | index-><a> -> .text
     timestamp = i.td.text
-    print(timestamp + ": " + text)
 
-df = pd.DataFrame()
+    temp = [timestamp, text]
+    tempList.append(temp)
+
+    #print(timestamp + ": " + text)
+
+
+vader = SentimentIntensityAnalyzer()
+df = pd.DataFrame(tempList, columns=['Date', 'Text'])
+
+#print(df['Text'][0])
+
+for i in range(len(df)):
+    text = (df['Text'][i])
+#    print(vader.polarity_scores(text))
+
+plt.figure(figsize=(10, 8))
+
+
+
